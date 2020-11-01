77450357a186feda1ee31ce4300407241e657d44
Rob_McBryde <robert.mcbryde@ros.gov.uk>
2020-11-01 16:48:00 +0000

    arabic to urnfield conversion via TDD

--- src/starter.js
+++ src/starter.js
@@ -1,1 +1,59 @@
-export const add = (a, b) => a + b;
+
+export const language = {
+    URNFIELD: 'urnfield',
+    ROMAN: 'roman',
+    ARABIC: 'arabic'
+}
+
+export const convert = (range = {start: 1, end: 10}, language = 'arabic') => {
+    switch(language) {
+        case 'urnfield':
+            return convertToUrnfield(range);
+        default:
+            return convertToArabic(range);
+           
+    }
+}
+
+const convertToUrnfield = range => {
+    let current = [];
+    for (let i = range.start; i <= range.end; i++) {
+         current.push(aracbicCharacterToUrnfield(i));
+    }
+    return current.join(', ');
+}
+
+const aracbicCharacterToUrnfield = (character) => {
+    let fives = [];
+    let other;
+    while (character >= 5) {
+        fives.push('\\');
+        character = character - 5;
+    }
+    switch (character) {
+        case 4:
+            other =  '////';
+            break;
+        case 3:
+            other =  '///';
+            break;
+        case 2:
+            other = '//';
+            break
+        case 1:
+            other = '/';
+            break;
+        default:
+            other = '';
+    }
+    const result = other + '' + fives.join('');
+    return result;
+}
+
+const convertToArabic = range => {
+    let result = [];
+    for (let i = range.start; i <= range.end; i++) {
+        result.push(i);
+    }
+    return result.join(', ');
+}
--- src/starter.test.js
+++ src/starter.test.js
@@ -1,6 +1,27 @@
-import { TestScheduler } from 'jest';
-import { add } from './starter';
+import { convert, language } from "./starter";
 
-test('add can sum two numbers', () => {
-    expect(add(3, 7)).toBe(10);
+test("can print numbers 1 to 10", () => {
+  expect(convert()).toBe("1, 2, 3, 4, 5, 6, 7, 8, 9, 10");
+});
+
+test("can print numbers 10 to 20", () => {
+  expect(convert({ start: 10, end: 20 })).toBe(
+    "10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20"
+  );
+});
+
+test("can convert Arabic 1 to Urnfield", () => {
+  expect(convert({ start: 1, end: 1 }, language.URNFIELD)).toBe("/");
+});
+
+test("can convert Arabic 1...10 to Urnfield", () => {
+  expect(convert({ start: 1, end: 10 }, language.URNFIELD)).toBe(
+    "/, //, ///, ////, \\, /\\, //\\, ///\\, ////\\, \\\\"
+  );
+});
+
+test("can convert Arabic 11...29 to Urnfield", () => {
+  expect(convert({ start: 11, end: 29 }, language.URNFIELD)).toBe(
+    "/\\\\, //\\\\, ///\\\\, ////\\\\, \\\\\\, /\\\\\\, //\\\\\\, ///\\\\\\, ////\\\\\\, \\\\\\\\, /\\\\\\\\, //\\\\\\\\, ///\\\\\\\\, ////\\\\\\\\, \\\\\\\\\\, /\\\\\\\\\\, //\\\\\\\\\\, ///\\\\\\\\\\, ////\\\\\\\\\\"
+  );
 });
