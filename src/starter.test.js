8f59a36e8a7dd52709ae1e5e4a600410d5994772
Rob_McBryde <robert.mcbryde@ros.gov.uk>
2020-11-04 16:02:25 +0000

    initial setup of the kata rules

--- src/starter.test.js
+++ src/starter.test.js
@@ -1,6 +1,22 @@
-import { TestScheduler } from 'jest';
-import { add } from './starter';
 
-test('add can sum two numbers', () => {
-    expect(add(3, 7)).toBe(10);
+/**
+ *      Text Speak 
+ *      =============
+ * 
+ *      Create a program that takes a string input and outputs the "cooler" text message equalivent from the early 2000s! 
+ * 
+ *      Rules
+ *      ============= 
+ *      1) All occurences of the letter 'e' should be replaced with the number '3'.
+ *      2) All occurences of the letter 'a' should be replaced with the number '4'.
+ *      3) All occurences of the letter 'l' should be replaced with the number '1'. 
+ *      4) If a word ends in the letter 's' then the final letter 's' should be replaced by the letter 'z'.
+ * 
+ *      13tz h4v3 som3 fun lolz
+ * 
+ */
+
+
+test('tests are running', () => {
+    expect(1).toBe(1);
 });
